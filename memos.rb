98da4b635a22b895a9f622516c83e65ec99d27e3
udaikue <ikueikue@gmail.com>
2022-05-26 08:40:18 +0900

    Implement index page

---
+++ memos.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader'
+
+before do
+  open('data/memos.json') do |m|
+    @memos_json = JSON.load(m)
+  end
+end
+
+get '/memos' do
+  erb :index
+end
