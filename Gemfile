e772373f1d7aee56f483f22affad8ecba27720f2
udaikue <ikueikue@gmail.com>
2022-05-26 10:48:44 +0900

    Install RSpec and add test

--- Gemfile
+++ Gemfile
@@ -6,3 +6,7 @@ gem 'sinatra'
 gem 'sinatra-contrib'
 gem 'thin'
 
+group :test do
+  gem 'rspec'
+  gem 'rack-test'
+end
---
+++ spec/memos_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative '../memos'
+
+RSpec.describe 'memos' do
+  it "shows title of memos" do
+    get '/memos'
+    expect(last_response.status).to eq(200)
+    expect(last_response.body).to include('メモ1', 'メモ2')
+  end
+end
