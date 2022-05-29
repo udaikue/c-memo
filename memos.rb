8c48d97234042fde3e3aaf447f7ae59b6c52d397
udaikue <ikueikue@gmail.com>
2022-05-29 10:37:41 +0900

    Implement edit function

--- memos.rb
+++ memos.rb
@@ -21,8 +21,12 @@ class Memo
     @connection.exec('INSERT INTO memos (title, content) VALUES ($1, $2);', [title, content])
   end
 
+  def self.update(id, title, content)
+    @connection.exec('UPDATE memos SET title=$1, content=$2 WHERE id=$3;', [title, content, id])
+  end
+
   def self.delete(id)
-    @connection.exec('DELETE FROM Memos WHERE id=$1;', [id])
+    @connection.exec('DELETE FROM memos WHERE id=$1;', [id])
   end
 
   def self.db_disconnect
@@ -51,11 +55,29 @@ get '/memos/new' do
 end
 
 get '/memos/:id' do
-  id = params[:id]
+  @id = params[:id]
   Memo.db_connect
-  @memo = Memo.details(id)
+  @memo = Memo.details(@id)
   Memo.db_disconnect
   erb :show
+end
+
+get '/memos/:id/edit' do
+  @id = params[:id]
+  Memo.db_connect
+  @memo = Memo.details(@id)
+  Memo.db_disconnect
+  erb :edit
+end
+
+patch '/memos/:id' do
+  id = params[:id]
+  title = params[:title]
+  content = params[:content]
+  Memo.db_connect
+  Memo.update(id, title, content)
+  Memo.db_disconnect
+  redirect to('/memos')
 end
 
 delete '/memos/:id' do
